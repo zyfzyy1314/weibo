0409385246c6d203e651c297acbbe1918706d945
Allen <807590643@.com>
2019-11-05 22:56:46 +0800

    Initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Allen
                 </div>
 
                 <div class="links">
